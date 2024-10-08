e4167806b08d498eb030d0903dcab649aef5aa9b
= <=>
2024-10-08 18:47:46 +0100

    documented

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -27,7 +27,7 @@ def pascal_triangle(n):
         for i in range(n - 1):
             temp = [0] + res[-1] + [0]
             row = []
-            for j in range(len(res[-1])):
+            for j in range(len(res[-1]) + 1):
                 row.append(temp[j] + temp[j + 1])
             res.append(row)
         return res
