32c373a207d418de9e37d1f460e7c1d2c8fdf42d
= <=>
2024-10-08 18:42:17 +0100

    0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
 
 def pascal_triangle(n):
+    """
+    Generate Pascal's triangle of height n.
+
+    Args:
+        n (int): The number of rows in Pascal's triangle.
+                  Must be a non-negative integer.
+
+    Returns:
+        List[List[int]]: A list of lists representing Pascal's triangle.
+                          Returns an empty list if n <= 0.
+
+    Example:
+        >>> pascal_triangle(5)
+        [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
+    """
     if n <= 0:
         return []
     else:
