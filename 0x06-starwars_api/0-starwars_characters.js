cbc1b6e6219c50006756fee809cd2b131ed1c7e9
ismailchiba <ismailchiba0@gmail.com>
2024-11-14 13:44:15 +0100

    semistandard style fixed

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,18 +6,18 @@ const movieId = process.argv[2];
 const movieURL = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
 request(movieURL, async (err, response, body) => {
-    if (err) {
-      console.log(err);
-    }
-    for (const characterId of JSON.parse(body).characters) {
-      await new Promise((resolve, reject) => {
-        request(characterId, (err, response, body) => {
-          if (err) {
-            reject(err);
-          }
-          console.log(JSON.parse(body).name);
-          resolve();
-        });
+  if (err) {
+    console.log(err);
+  }
+  for (const characterId of JSON.parse(body).characters) {
+    await new Promise((resolve, reject) => {
+      request(characterId, (err, response, body) => {
+        if (err) {
+          reject(err);
+        }
+        console.log(JSON.parse(body).name);
+        resolve();
       });
-    }
-  });
+    });
+  }
+});
