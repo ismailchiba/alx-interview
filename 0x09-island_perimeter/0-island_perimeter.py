ebe6d7d61da34f084008e8faf464124d32c692e2
ismailchiba <105497114+ismailchiba@users.noreply.github.com>
2024-12-05 16:59:02 +0100

    Update 0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""
-Island_perimeter problem
+"""Island Perimeter Problem
 """
 
 
-def island_perimeter(grid: list[list[int]]) -> int:
+def island_perimeter(grid):
     """
     Calculates the perimeter of the island described in grid
     Args:
@@ -13,21 +12,16 @@ def island_perimeter(grid: list[list[int]]) -> int:
         the perimeter of the island
     """
 
-    perimeter = 0
-    rows = len(grid)
-    cols = len(grid[0])
-
-    def dfs(r, c):
-        if r < 0 or r >= rows or c < 0\
-                or c >= cols or grid[r][c] == 0:
-            return 1
-        if grid[r][c] == 1:
-            grid[r][c] = 2
-            return dfs(r-1, c) + dfs(r+1, c)\
-                + dfs(r, c-1) + dfs(r, c+1)
-        return 0
-    for r in range(rows):
-        for c in range(cols):
-            if grid[r][c] == 1:
-                perimeter += dfs(r, c)
-    return perimeter
+    p = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (grid[i][j] == 1):
+                if (i <= 0 or grid[i - 1][j] == 0):
+                    p += 1
+                if (i >= len(grid) - 1 or grid[i + 1][j] == 0):
+                    p += 1
+                if (j <= 0 or grid[i][j - 1] == 0):
+                    p += 1
+                if (j >= len(grid[i]) - 1 or grid[i][j + 1] == 0):
+                    p += 1
+    return p
